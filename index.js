013b7e322c016db975f9cacaec1a4e658be5023d
MutaiNewton56 <mutainewton56.gmail.com>
2024-04-05 12:04:11 +0300

    using array reduce

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const batteries = [4, 5, 3, 7, 2, 10];
+
+const reducer = (batteryTotal, currentBattery) => batteryTotal + currentBattery;
+
+const totalBatteries = batteries.reduce(reducer, 0);
+
+console.log(totalBatteries); // Output: 31
